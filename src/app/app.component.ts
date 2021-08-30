e87bcd26667451cd978f19fce55d47153e108e00
sunny <jjohye11@gmail.com>
2021-08-31 00:39:28 +0900

    sqlite 테스트

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,6 @@
+import { Storage } from '@ionic/storage-angular';
+import * as CordovaSQLiteDriver from 'localforage-cordovasqlitedriver';
+import { Drivers } from '@ionic/storage';
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
@@ -5,6 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  public test = 'test_login';
   public appPages = [
     { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
     { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
@@ -14,5 +18,26 @@ export class AppComponent {
     { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  constructor(private storage: Storage
+    ) {
+      this.init();
+    }
+
+    async init() {
+      console.log('init');
+      await this.storage.defineDriver(CordovaSQLiteDriver);
+      await this.storage.create();
+    }
+
+    getValue() {
+      this.storage.get(this.test).then(value => console.log('get result: ', value));
+    }
+
+    setValue() {
+      return this.storage.set(this.test, 'hyesun3');
+    }
+
+    clearValue() {
+      return this.storage.clear();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,18 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import * as CordovaSQLiteDriver from 'localforage-cordovasqlitedriver';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { Drivers, Storage } from '@ionic/storage';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), IonicStorageModule.forRoot({
+    name: '_sunny_',
+    // eslint-disable-next-line no-underscore-dangle
+    driverOrder: [CordovaSQLiteDriver._driver]
+  }),AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
